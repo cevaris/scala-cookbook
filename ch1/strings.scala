adca437826594775ba4654fa51eca3e7a0b913a1
Cevaris <cevaris@gmail.com>
2014-05-18 13:30:47 -0700

    working on strings

--- ch1/strings.scala
+++ ch1/strings.scala
@@ -1,21 +1,100 @@
 // "hello".foreach(println)
 
+
+
+
 // for(c <- "hello") println(c)
+
+
+
 
 // val s = "hello"
 // s.getBytes.foreach(println)
 
+
+
+
 // val result = "hello".filter( _ != 'l' )
 // println(result)
+
+
+
 
 // val result = "hello".take(2)
 // println(result)
 
+
+
+
 // val result = "hello".drop(2)
 // println(result)
+
+
 
 
 // val left  = "hello"
 // val right = "HEllO"
 // println(left == right)
 // println(left.toUpperCase == right.toUpperCase)
+// println(left.equalsIgnoreCase(right))
+
+
+
+
+// val foo = """This 
+// is 
+// a 
+
+// multiline 
+// String that is preserved"""
+// println(foo)
+
+
+
+
+
+
+// "hello world".split(" ").foreach(println)
+// "hello, world".split(',').map(_.trim).foreach(println)
+
+
+
+
+
+val name = "Adam"
+val age = 20
+val weight = 200.3
+println(s"$name is $age years old, and weighs $weight pounds.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
