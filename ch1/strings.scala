8803dd370b3be2bbed164aa10e15fd93487bdd59
Cevaris <cevaris@gmail.com>
2014-05-18 12:58:22 -0700

    looking at strings

---
+++ ch1/strings.scala
@@ -0,0 +1,21 @@
+// "hello".foreach(println)
+
+// for(c <- "hello") println(c)
+
+// val s = "hello"
+// s.getBytes.foreach(println)
+
+// val result = "hello".filter( _ != 'l' )
+// println(result)
+
+// val result = "hello".take(2)
+// println(result)
+
+// val result = "hello".drop(2)
+// println(result)
+
+
+// val left  = "hello"
+// val right = "HEllO"
+// println(left == right)
+// println(left.toUpperCase == right.toUpperCase)
