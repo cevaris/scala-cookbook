9ea84eb18806173f7b89b2e475f77a968ae65fb2
Cevaris <cevaris@gmail.com>
2014-05-20 17:04:19 -0600

    Exception handling

--- ch2/numbers.scala
+++ ch2/numbers.scala
@@ -32,24 +32,64 @@
 // calcPi(1*100*100, x => printf(s"\r$x"))
 
 
-def calcPi(iterations:Int): BigDecimal  = {
-  var estimate = BigDecimal(3)
-  var position = 2
+// def calcPi(iterations:Int): BigDecimal  = {
+//   var estimate = BigDecimal(3)
+//   var position = 2
 
-  for( iter <- 0 to iterations ){
+//   for( iter <- 0 to iterations ){
 
-    val step = BigDecimal(4.0)/BigDecimal(BigInt(position)*BigInt(position+1)*BigInt(position+2))
+//     val step = BigDecimal(4.0)/BigDecimal(BigInt(position)*BigInt(position+1)*BigInt(position+2))
 
-    iter % 2 match {
-      case 0 => estimate += step
-      case _ => estimate -= step
+//     iter % 2 match {
+//       case 0 => estimate += step
+//       case _ => estimate -= step
+//     }
+
+//     position = position+2
+//     printf(s"\r$estimate")
+//   }
+//   println()
+//   estimate
+// }
+
+// calcPi(1*100*100)
+
+
+
+
+
+
+
+
+
+
+implicit class StringToInt(s: String) {
+  def parseInt():Option[Int] = { 
+    try {
+      Some(s.toInt) 
+    } catch {
+    case e: NumberFormatException => None 
     }
-
-    position = position+2
-    printf(s"\r$estimate")
   }
-  println()
-  estimate
 }
 
-calcPi(1*100*100)
+"100".parseInt() match {
+  case Some(n) => println(n)
+  case None => println("Boom! That wasn't a number.")
+}
+
+"100+".parseInt() match {
+  case Some(n) => println(n)
+  case None => println("Boom! That wasn't a number.")
+}
+
+"-1001".parseInt() match {
+  case Some(n) => println(n)
+  case None => println("Boom! That wasn't a number.")
+}
+
+
+
+
+
+
