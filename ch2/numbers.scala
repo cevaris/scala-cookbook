560ff308d78445fe247aa6306f0dfa7be870bf04
Cevaris <cevaris@gmail.com>
2014-05-20 16:24:35 -0600

    created function to calculate pi

--- ch2/numbers.scala
+++ ch2/numbers.scala
@@ -9,29 +9,28 @@
 
 
 
-val iterations = 50*1000*1000
-var estimate = BigDecimal(3)
-var position = BigInt(2)
+def calcPi(iterations:Int) = {
+  var estimate = BigDecimal(3)
+  var position = BigInt(2)
 
-for( iter <- 0 to iterations){
+  for( iter <- 0 to iterations){
 
-  val d0:BigInt = position
-  val d1:BigInt = position+1
-  val d2:BigInt = position+2
-  position      = position+2
+    val d0:BigInt = position
+    val d1:BigInt = position+1
+    val d2:BigInt = position+2
+    position      = position+2
 
-  // println(s"$d0 $d1 $d2 $position ${d0*d1*d2}")
-  // println(d0*d1*d2)
-  val step = BigDecimal(4.0)/BigDecimal(d0*d1*d2)
-  // println(step)
+    val step = BigDecimal(4.0)/BigDecimal(d0*d1*d2)
 
-  if(iter % 2 == 0){
-    estimate += step
-  } else {
-    estimate -= step
-  }
+    if(iter % 2 == 0){
+      estimate += step
+    } else {
+      estimate -= step
+    }
 
-  // println(estimate)
-  printf(s"\r$estimate")
+    printf(s"\r$estimate")
   
+  }
 }
+
+calcPi(1*100*100)
